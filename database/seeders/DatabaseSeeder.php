8a3798a3f57e8549cfd0cf769081f1fd46188421
Eslam <eslamanwer67@gmail.com>
2025-04-21 18:08:22 +0200

    adding database seeder classes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Database\Seeders\CandidateAndCompanySeeder;
+use Database\Seeders\PassportClientsSeeder;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -18,6 +20,10 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+        $this->call([
+            CandidateAndCompanySeeder::class,
+            PassportClientsSeeder::class,
         ]);
     }
 }
